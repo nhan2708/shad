054e9f3d7529c7a4831c9b2d7f4ddfd6d8e8a53b
nhan2708 <163325777+nhan2708@users.noreply.github.com>
2025-06-22 20:05:16 +0700

    Delete js directory

--- js/deleteHeader.js
+++
@@ -1,7 +0,0 @@
-/***********************************************
-> deleteHeader by nhan2708
-***********************************************/
-function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}
-var modifiedHeaders=$request.headers;
-setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag","");
-$done({headers:modifiedHeaders});
