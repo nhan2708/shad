78173e86885da13f2e6a7dc8fd32f094a1ef8fc4
nhan2708 <163325777+nhan2708@users.noreply.github.com>
2025-06-23 21:19:51 +0700

    Update Locket_VTN.js

--- js/Locket_VTN.js
+++ js/Locket_VTN.js
@@ -1,12 +1,3 @@
-
-const mapping = {
-  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
-  'Locket': ['Gold']
-};
-
-
-
-const ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
 const obj = JSON.parse($response.body);
 
 obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
@@ -19,21 +10,21 @@ const subscriptionInfo = {
   expires_date: "2099-12-18T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "2024-08-27T01:04:18Z",
-  purchase_date: "2024-08-27T01:04:17Z",
+  original_purchase_date: "2024-07-28T01:04:18Z",
+  purchase_date: "2024-07-28T01:04:17Z",
   store: "app_store"
 };
 
 const entitlementInfo = {
   grace_period_expires_date: null,
-  purchase_date: "2024-08-27T01:04:17Z",
-  product_identifier: "com.nhan2708.premium.yearly",
+  purchase_date: "2024-07-28T01:04:17Z",
+  product_identifier: "com.ohoang7.premium.yearly",
   expires_date: "2099-12-18T01:04:17Z"
 };
 
-obj.subscriber.subscriptions["com.nhan2708.premium.yearly"] = subscriptionInfo;
+obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = subscriptionInfo;
 obj.subscriber.entitlements["gold"] = entitlementInfo;
 obj.subscriber.entitlements["record_long"] = entitlementInfo;
-obj.subscriber.entitlements["locket_gold_badge"] = entitlementInfo; // Bật công tắc Locket Gold badge
+obj.subscriber.entitlements["locket_gold_badge"] = entitlementInfo;
 
 $done({ body: JSON.stringify(obj) });
