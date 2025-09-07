f1a1a1cb289e3cb50371b54fbeccfc62ce7ac0d5
nhan2708 <163325777+nhan2708@users.noreply.github.com>
2025-09-07 09:29:58 +0700

    Update Locket_VTN.js

--- js/Locket_VTN.js
+++ js/Locket_VTN.js
@@ -1,4 +1,4 @@
-// ========= ID ========= //
+// ========= ID Mapping ========= //
 const mapping = {
   '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
   'Locket': ['Gold']
@@ -7,13 +7,15 @@ const mapping = {
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
 var obj = JSON.parse($response.body);
 
-// Định nghĩa thông tin thuê bao
-var ohoang7 = {
+// Thông báo cảnh báo
+obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
+
+var nhan2708 = {
   is_sandbox: false,
   ownership_type: "PURCHASED",
   billing_issues_detected_at: null,
   period_type: "normal",
-  expires_date: "2025-08-27T01:04:17Z",
+  expires_date: "2099-12-18T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
   original_purchase_date: "2025-08-27T01:04:18Z",
@@ -21,30 +23,27 @@ var ohoang7 = {
   store: "app_store"
 };
 
-var vuong2023 = {
+var nhan2708_sub = {
   grace_period_expires_date: null,
   purchase_date: "2025-08-27T01:04:17Z",
   product_identifier: "com.ohoang7.premium.yearly",
-  expires_date: "2025-08-27T01:04:17Z"
+  expires_date: "2099-12-18T01:04:17Z"
 };
 
-// Xử lý theo mapping
-const match = Object.keys(mapping).find(key => ua.includes(key));
+const match = Object.keys(mapping).find(e => ua.includes(e));
 
 if (match) {
   let [entitlement, product] = mapping[match];
-  
   if (product) {
-    vuong2023.product_identifier = product;
-    obj.subscriber.subscriptions[product] = ohoang7;
+    nhan2708_sub.product_identifier = product;
+    obj.subscriber.subscriptions[product] = nhan2708;
   } else {
-    obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = ohoang7;
+    obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = nhan2708;
   }
-
-  obj.subscriber.entitlements[entitlement] = vuong2023;
+  obj.subscriber.entitlements[entitlement] = nhan2708_sub;
 } else {
-  obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = ohoang7;
-  obj.subscriber.entitlements.pro = vuong2023;
+  obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = nhan2708;
+  obj.subscriber.entitlements.pro = nhan2708_sub;
 }
 
 $done({ body: JSON.stringify(obj) });
